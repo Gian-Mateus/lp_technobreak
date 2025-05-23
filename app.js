fd6dab10279c625b229ae5c7c16001ec3fb91b78
Gian Mateus <gian.m.silver@gmail.com>
2025-05-23 16:54:09 -0300

    feat: carousel animate

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-const btnOpen  = document.querySelector('.button-open')
-const btnClose = document.querySelector('.button-close')
-const menu     = document.getElementById('menu')
+const btnOpen  = document.querySelector('.button-open');
+const btnClose = document.querySelector('.button-close');
+const menu     = document.getElementById('menu');
 
 // Ao clicar em "open"
 btnOpen.addEventListener('click', () => {
-    menu.classList.remove('w-0')
-    menu.classList.add('w-48')     // 12rem ≈ 11.5rem desejado
+    menu.classList.remove('w-0');
+    menu.classList.add('w-48');     // 12rem ≈ 11.5rem desejado
 
-    btnOpen.classList.add('hidden')
-    btnClose.classList.remove('hidden')
+    btnOpen.classList.add('hidden');
+    btnClose.classList.remove('hidden');
 })
 
 // Ao clicar em "close"
 btnClose.addEventListener('click', () => {
-    menu.classList.add('w-0')
-    menu.classList.remove('w-48')
+    menu.classList.add('w-0');
+    menu.classList.remove('w-48');
 
-    btnClose.classList.add('hidden')
-    btnOpen.classList.remove('hidden')
+    btnClose.classList.add('hidden');
+    btnOpen.classList.remove('hidden');
 })
+
+// Carousel
+
+//const carousel = document.getElementById('hero-heading');
+const items = document.querySelectorAll('#hero-heading .slide');
+// items[index].classList.add('active-slide');
+
+// setInterval(() => {
+    //     items[index].classList.remove('active-slide');
+    //     index = (index + 1) % items.length
+    //     items[index].classList.add('active-slide')
+    // }, 3000)
+
+let index = 0;
+setInterval(() => {
+    items.forEach((item) => {
+        item.style.transform = `translateX(-${index}00%)`;
+    })
+
+    if (index === items.length - 1) {
+        index = 0;
+    } else{
+        index++;
+    }
+}, 2000)
